51989ab6e993f84406d788a9c18110c931e7db93
MohamedRiyaz1844 <148085481+MohamedRiyaz1844@users.noreply.github.com>
2023-10-26 22:43:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+    const errorMessage = document.getElementById('error-message');
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // You can add your authentication logic here (e.g., checking against hardcoded values).
+        if (username === 'riyaz' && password === 'password') {
+            // Redirect to the desired page after successful login.
+            window.location.href = 'index.html'; // Change 'dashboard.html' to the desired URL.
+        } else {
+            errorMessage.textContent = 'Invalid username or password. Please try again.';
+        }
+    });
+});
